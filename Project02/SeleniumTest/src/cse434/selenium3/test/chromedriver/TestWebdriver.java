d835c20a894fdc9d288dcab647edb45aec3ff3e7
MdMonirHossan <mdraju01820@gmail.com>
2019-08-06 22:23:12 +0600

    Set up chrome driver to run selenium test

--- Project02/SeleniumTest/src/cse434/selenium3/test/chromedriver/TestWebdriver.java
+++ Project02/SeleniumTest/src/cse434/selenium3/test/chromedriver/TestWebdriver.java
@@ -1,10 +1,31 @@
 package cse434.selenium3.test.chromedriver;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestWebdriver {
+	
+	static WebDriver driver;
 
 	public static void main(String[] args) {
 		
-
+		try {
+			System.setProperty("webdriver.chrome.driver", "/Users/MDMONIRHOSSAN/Documents/CSE434/Project02/chromedriver");
+			driver = new ChromeDriver();
+			driver.manage().deleteAllCookies();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+			
+			driver.get("https://google.com");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
