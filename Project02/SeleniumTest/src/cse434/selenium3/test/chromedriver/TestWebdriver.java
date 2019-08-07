4df56ee6a09f443cf24612f3c133f8c1a1b0c152
MdMonirHossan <mdraju01820@gmail.com>
2019-08-07 21:00:44 +0600

    Test google search for selenium

--- Project02/SeleniumTest/src/cse434/selenium3/test/chromedriver/TestWebdriver.java
+++ Project02/SeleniumTest/src/cse434/selenium3/test/chromedriver/TestWebdriver.java
@@ -2,6 +2,7 @@ package cse434.selenium3.test.chromedriver;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,6 +11,15 @@ public class TestWebdriver {
 	static WebDriver driver;
 
 	public static void main(String[] args) {
+		
+		TestWebdriver test = new TestWebdriver();
+		test.invokeBrowser();
+		
+		
+	}
+	
+	public void invokeBrowser() {
+
 		
 		try {
 			System.setProperty("webdriver.chrome.driver", "/Users/MDMONIRHOSSAN/Documents/CSE434/Project02/chromedriver");
@@ -20,7 +30,25 @@ public class TestWebdriver {
 			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 			
 			driver.get("https://google.com");
+			
+			setInputField();
+			
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	
+	}
+	
+	public static void setInputField() {
+		
+		try {
+			driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys("7 website for selenium testing");
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[2]")).click();
+		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
