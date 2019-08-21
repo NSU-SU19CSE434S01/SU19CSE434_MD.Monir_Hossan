016257c29dee24dfc8167ea2764daa9585af89b4
MdMonirHossan <mdraju01820@gmail.com>
2019-08-21 12:13:41 +0600

    Add PHPTravel and some selenium test

--- Project02/SeleniumTest/src/cse434/selenium3/test/chromedriver/TestWebdriver.java
+++ Project02/SeleniumTest/src/cse434/selenium3/test/chromedriver/TestWebdriver.java
@@ -3,8 +3,12 @@ package cse434.selenium3.test.chromedriver;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+
+
 
 public class TestWebdriver {
 	
@@ -25,11 +29,11 @@ public class TestWebdriver {
 			System.setProperty("webdriver.chrome.driver", "/Users/MDMONIRHOSSAN/Documents/CSE434/Project02/chromedriver");
 			driver = new ChromeDriver();
 			driver.manage().deleteAllCookies();
-			driver.manage().window().maximize();
+			//driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 			
-			driver.get("https://google.com");
+			driver.get("https://www.phptravels.net");
 			
 			setInputField();
 			
@@ -45,9 +49,40 @@ public class TestWebdriver {
 	public static void setInputField() {
 		
 		try {
-			driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys("7 website for selenium testing");
+			driver.findElement(By.xpath("//*[@id=\"body-section\"]/section/div[2]/div/div/div[2]/ul/li[2]/a")).click();
 			Thread.sleep(2000);
-			driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[2]")).click();
+			driver.findElement(By.xpath("//*[@id=\"s2id_origin\"]/a/span[1]")).click();
+			driver.findElement(By.xpath("/html/body/div[18]/div/input")).sendKeys("jfk");
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("/html/body/div[18]/div/input")).sendKeys(Keys.RETURN);
+			
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"s2id_destination\"]/a/span[1]")).click();
+			driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/div/input")).sendKeys("washington");
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/div/input")).sendKeys(Keys.RETURN);
+			
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"departure\"]")).click();
+			//driver.findElement(By.xpath("//*[@id=\"departure\"]")).clear();
+			Thread.sleep(1000);
+			
+			driver.findElements(By.cssSelector
+		            ("body > div:nth-child(21) > div.datepicker-days > table > tbody > tr:nth-child(5) > td:nth-child(7)")).get(0).click();
+			
+			//driver.findElement(By.xpath("//*[@id=\"departure\"]")).sendKeys("2019-10-12");
+//			Thread.sleep(1000);
+//			driver.findElement(By.xpath("//*[@id=\"departure\"]")).sendKeys(Keys.RETURN);
+			
+			//*[@id="departure"]
+			
+			
+			
+			driver.close();
+			
+			
+
+			
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
