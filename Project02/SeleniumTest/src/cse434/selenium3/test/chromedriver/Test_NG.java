bfdabb3a8ba83d7fc6c0d5aef59c0922e04ec13b
MdMonirHossan <mdraju01820@gmail.com>
2019-08-24 16:29:31 +0600

    Creating TestNG class & Test output

---
+++ Project02/SeleniumTest/src/cse434/selenium3/test/chromedriver/Test_NG.java
@@ -0,0 +1,14 @@
+package cse434.selenium3.test.chromedriver;
+
+import org.testng.annotations.Test;
+
+public class Test_NG {
+
+	@Test
+	public void testWebdriver() {
+		
+		System.out.println("Webdriver test");
+	}
+	
+	
+}
