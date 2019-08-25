04ea89ad5eaf42f3118ebd7aef9e3d58bc166fda
MdMonirHossan <mdraju01820@gmail.com>
2019-08-25 23:44:22 +0600

    Add one test for login

--- Project02/SeleniumTest/src/cse434/selenium3/test/chromedriver/Test_NG.java
+++ Project02/SeleniumTest/src/cse434/selenium3/test/chromedriver/Test_NG.java
@@ -2,6 +2,7 @@ package cse434.selenium3.test.chromedriver;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -31,16 +32,37 @@ public class Test_NG {
 	@Test
 	public void testWebdriver() {
 		
-		//selenium.invokeBrowser();
+		try {
+			driver.get("https://www.phptravels.net/login");
+			
+			driver.findElement(By.xpath("//*[@id=\"loginfrm\"]/div[1]/div[5]/div/div[1]/input")).sendKeys("mdmonir@gmail.com");
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"loginfrm\"]/div[1]/div[5]/div/div[2]/input")).sendKeys("123456");
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"loginfrm\"]/div[1]/div[5]/div/label")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"loginfrm\"]/button")).click();
+			
+
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		System.out.println("Webdriver test");
 	}
 	
 	
 	@AfterTest
 	public void closeBrowser() {
 		
-		driver.close();
+		try {
+			Thread.sleep(3000);
+			driver.close();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
