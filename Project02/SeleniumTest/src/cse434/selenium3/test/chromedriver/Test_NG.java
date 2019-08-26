10fa89d106e6008dcf9a25cbb19d61968e62c0c6
MdMonirHossan <mdraju01820@gmail.com>
2019-08-26 10:07:09 +0600

    Test case for signup with Priority

--- Project02/SeleniumTest/src/cse434/selenium3/test/chromedriver/Test_NG.java
+++ Project02/SeleniumTest/src/cse434/selenium3/test/chromedriver/Test_NG.java
@@ -29,13 +29,63 @@ public class Test_NG {
 		driver.get("https://www.phptravels.net");
 	}
 	
-	@Test
-	public void testWebdriver() {
+	@Test(priority=1)
+	public void signUp() {
+		
+		try {
+
+			driver.get("https://www.phptravels.net/register");
+			
+			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[3]/input")).sendKeys("Md Monir");
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[4]/input")).sendKeys("Hossan");
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[5]/input")).sendKeys("01782345064");
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[6]/input")).sendKeys("monir@gmail.com");
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[7]/input")).sendKeys("123456");
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[8]/input")).sendKeys("123456");
+			Thread.sleep(1000);
+			jse = (JavascriptExecutor)driver;
+			jse.executeScript("scroll(0,200)");
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[9]/button")).click();
+			Thread.sleep(1000);
+			jse = (JavascriptExecutor)driver;
+			jse.executeScript("scroll(0,-500)");
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[6]/input")).clear();
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[6]/input")).sendKeys("mdmonirh@gmail.com");
+			Thread.sleep(1000);
+			jse = (JavascriptExecutor)driver;
+			jse.executeScript("scroll(0,200)");
+			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[9]/button")).click();
+			Thread.sleep(3000);
+			
+			driver.get("https://www.phptravels.net/account/logout"); 
+			
+//			Select select = new Select(driver.findElement(By.cssSelector("body > nav > div > div.collapse.navbar-collapse > ul.nav.navbar-nav.navbar-right.hidden-sm.go-left > ul > li:nth-child(1) > a")));
+//			Thread.sleep(1000);
+//			select.selectByIndex(1);
+//			Thread.sleep(1000);
+			
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test(priority=2)
+	public void logIn() {
 		
 		try {
 			driver.get("https://www.phptravels.net/login");
 			
-			driver.findElement(By.xpath("//*[@id=\"loginfrm\"]/div[1]/div[5]/div/div[1]/input")).sendKeys("mdmonir@gmail.com");
+			driver.findElement(By.xpath("//*[@id=\"loginfrm\"]/div[1]/div[5]/div/div[1]/input")).sendKeys("monir@gmail.com");
 			Thread.sleep(1000);
 			driver.findElement(By.xpath("//*[@id=\"loginfrm\"]/div[1]/div[5]/div/div[2]/input")).sendKeys("123456");
 			Thread.sleep(1000);
--- Project02/Test/src/testng/NewTest.java
+++
@@ -1,9 +0,0 @@
-package testng;
-
-import org.testng.annotations.Test;
-
-@Test
-public class NewTest {
-  public void f() {
-  }
-}
